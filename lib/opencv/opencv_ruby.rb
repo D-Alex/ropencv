eb46ea6ce6ebc28bdcf744eca6d7c61ca1aa19e1
Alexander Duda <Alexander.Duda@dfki.de>
2013-06-23 20:37:12 +0200

    remove LibC
    this is no longer needed

--- lib/opencv/opencv_ruby.rb
+++ lib/opencv/opencv_ruby.rb
@@ -1,20 +1,4 @@
 module OpenCV
-    module LibC
-        extend FFI::Library
-        ffi_lib FFI::Library::LIBC
-
-        # memory allocators
-        attach_function :malloc, [:size_t], :pointer
-        attach_function :calloc, [:size_t], :pointer
-        attach_function :valloc, [:size_t], :pointer
-        attach_function :realloc, [:pointer, :size_t], :pointer
-        attach_function :free, [:pointer], :void
-
-        # memory movers
-        attach_function :memcpy, [:pointer, :pointer, :size_t], :pointer
-        attach_function :bcopy, [:pointer, :pointer, :size_t], :void
-    end # module LibC
-
     def self.included(obj)
         obj.class_eval do 
             def cv
