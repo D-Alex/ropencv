7a6e88739b67c53b7379cb3195cc7d73f8d21272
Alexander Duda <Alexander.Duda@dfki.de>
2013-10-28 11:10:30 +0100

    cleanup match_keypoint to run stand-alone

---
+++ examples/match_keypoints.rb
@@ -0,0 +1,33 @@
+require 'ropencv'
+include OpenCV
+
+img1 = cv::imread('tsukuba_l.png',CV_LOAD_IMAGE_GRAYSCALE)
+img2 = cv::imread('tsukuba_r.png',CV_LOAD_IMAGE_GRAYSCALE)
+if (img1.empty() || img2.empty())
+    puts("Can't read one of the images\n")
+    return -1
+end
+
+keypoints1=Vector.new(cv::KeyPoint)
+keypoints2=Vector.new(cv::KeyPoint)
+detector = cv::FeatureDetector::create('SURF')
+detector.detect(img1, keypoints1)
+detector.detect(img2, keypoints2)
+
+# computing descriptors
+extractor=cv::DescriptorExtractor::create('SURF')
+descriptors2=cv::Mat.new(3, 4, cv::CV_64FC1)
+descriptors1=cv::Mat.new(3, 4, cv::CV_64FC1)
+extractor.compute(img1, keypoints1, descriptors1)
+extractor.compute(img2, keypoints2, descriptors2)
+
+# matching descriptors
+matcher= cv::BFMatcher::create('BruteForce-L1')
+matches= Vector.new(cv::DMatch)
+matcher.match(descriptors1, descriptors2, matches)
+
+# drawing the results
+img_matches=cv::Mat.new(3, 4, cv::CV_64FC1)
+cv::drawMatches(img1, keypoints1, img2, keypoints2, matches, img_matches)
+cv::imshow("matches", img_matches)
+cv::waitKey(0)
